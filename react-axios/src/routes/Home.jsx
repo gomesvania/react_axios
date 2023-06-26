c77363de25728264885480649f9b053ed35376ec
Vania Gomes - Bora Codar <vania.vgomes@mercadolivre.com>
2023-06-26 16:32:49 -0300

    Consume API data with React and Axios

---
+++ react-axios/src/routes/Home.jsx
@@ -0,0 +1,46 @@
+import blogFetch from "../axios/config";
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+
+import './Home.css'
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const reponse = await blogFetch.get("/posts");
+
+      const data = reponse.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link className="btn" to={`/posts/${post.id}`}>
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Home
